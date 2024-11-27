45f9b53df1972581b6e5b36b7b4acc54906b7d51
10senura <senuraindeewara@gmail.com>
2024-11-27 16:36:55 +0530

    add cards

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 
-let movies = document.getElementById("searchmovie").value;
 
-let res = await fetch('https://dummyapi.online/api/movies')
-.then((response) => response.json())
-.then((json) => {console.log(json)
-  
-});
-    $.ajax({
-      type: "GET",
-      url: "https://dummyapi.online/api/movies",
-      data: "data",
-      dataType: "json",
-      success: function (response) {
-          console.log(response);
-      }
+//---------------------------------------------------------------------------------------------------------
+
+
+function fetchMovies() {
+  const movieList = document.getElementById('movieList'); // Make sure this exists in your HTML
+  movies.forEach(movie => {
+    const movieCard = `
+      <div class="col-md-4">
+        <div class="card mb-4">
+          <img src="${movie.image}" class="card-img-top" alt="${movie.title}">
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">${movie.description}</p>
+            <a href="${movie.url}" class="btn btn-primary" target="_blank">More Info</a>
+          </div>
+        </div>
+      </div>
+    `;
+    movieList.innerHTML += movieCard;
   });
+}
+
+document.getElementById('fetchMovies').addEventListener('click', fetchMovies);
